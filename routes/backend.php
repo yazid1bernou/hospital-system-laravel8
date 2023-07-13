003793c6d1ad3ac6e9299cc00aa5d80c66e38d46
unknown <yazid1bernou@gmail.com>
2023-07-13 09:04:12 +0000

    changed and configurated the route dashboard user

--- routes/backend.php
+++ routes/backend.php
@@ -13,3 +13,8 @@ use App\Http\Controllers\Dashboard\DashboardController;
 
 Route::get('/Admin_Dashboard' , [DashboardController::class , 'index']);
 
+Route::get('/dashboard/user', function () {
+    return view('Dashboard.User.dashboard');
+})->middleware(['auth'])->name('dashboard.user');
+
+require __DIR__.'/auth.php';
