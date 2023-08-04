957e9b66e9055e6a1deb4574529140c61a413921
unknown <yazid1bernou@gmail.com>
2023-08-04 11:36:50 +0000

    Update style RTL and LTR package mcamara

--- routes/backend.php
+++ routes/backend.php
@@ -13,12 +13,24 @@ use App\Http\Controllers\Dashboard\DashboardController;
 
 Route::get('/Admin_Dashboard' , [DashboardController::class , 'index']);
 
-Route::get('/dashboard/user', function () {
-    return view('Dashboard.User.dashboard');
-})->middleware(['auth'])->name('dashboard.user');
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){ 
 
-Route::get('/dashboard/admin', function () {
-    return view('Dashboard.Admin.dashboard');
-})->middleware(['auth:admin'])->name('dashboard.admin');
+        Route::get('/dashboard/user', function () {
+            return view('Dashboard.User.dashboard');
+        })->middleware(['auth'])->name('dashboard.user');
+        
+        Route::get('/dashboard/admin', function () {
+            return view('Dashboard.Admin.dashboard');
+        })->middleware(['auth:admin'])->name('dashboard.admin');
+        
+        require __DIR__.'/auth.php';   
+        
+    });
 
-require __DIR__.'/auth.php';   
+    
+
+
